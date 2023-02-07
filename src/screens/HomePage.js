6c66e2ac16f1d7d98969d6b70cd700c3fd041b83
dev2820 <second28200@gmail.com>
2023-02-07 21:29:54 +0900

    feature/ start버튼 스타일 수정

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,7 +1,9 @@
-import { StyleSheet, Button } from "react-native";
-import { Page } from "@components/views";
+import { StyleSheet, Text } from "react-native";
+import { Page, Card } from "@components/views";
 import Title from "@components/Title";
 import Settings from "@components/Settings";
+import globalStyle from "@assets/globalStyle";
+import THEME from "@constants/THEME";
 
 const HomePage = ({ navigation }) => {
   const gotoTimer = () => {
@@ -9,24 +11,34 @@ const HomePage = ({ navigation }) => {
   };
 
   return (
-    <Page>
+    <Page style={styles.container}>
       <Title>🔥 TBT 🔥</Title>
-      <Settings />
-      <Button
-        title="start"
-        style={[styles.startButton]}
-        onPress={gotoTimer}
-      ></Button>
+      <Settings
+        style={{
+          height: 380,
+          width: "100%",
+        }}
+      />
+      <Card style={[styles.startButton]} onPress={gotoTimer}>
+        <Text style={[globalStyle.HEADING_LARGE, globalStyle.ON_PRIMARY]}>
+          START
+        </Text>
+      </Card>
     </Page>
   );
 };
 
 const styles = StyleSheet.create({
-  startButton: {
-    width: 160,
-    height: 160,
-    elevation: 6,
+  container: {
     alignItems: "center",
+  },
+  startButton: {
+    width: 340,
+    height: 80,
+    elevation: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: THEME.PRIMARY,
   },
 });
 

f8786d90dc22d7c2d8146ca4006a9cd69e0a6c24
dev2820 <second28200@gmail.com>
2023-02-07 23:12:03 +0900

    feature/centerModal 뷰 생성

---
+++ src/components/views/CenterModal.js
@@ -0,0 +1,37 @@
+import Modal from "react-native-modal";
+import { StyleSheet, View } from "react-native";
+
+const CenterModal = ({ isVisible, children, onBackdropPress }) => {
+  return (
+    <Modal
+      isVisible={isVisible}
+      coverScreen={true}
+      animationIn="fadeIn"
+      animationInTiming={200}
+      backdropTransitionInTiming={0}
+      animationOut="fadeOut"
+      backdropTransitionOutTiming={0}
+      animationOutTiming={200}
+      onBackdropPress={onBackdropPress}
+    >
+      <View style={styles.modalView}>{children}</View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalView: {
+    position: "absolute",
+    width: 300,
+    height: 360,
+    backgroundColor: "white",
+    left: "50%",
+    marginLeft: -150,
+    top: "50%",
+    marginTop: -180,
+    borderRadius: 16,
+    padding: 16,
+  },
+});
+
+export default CenterModal;
--- src/components/views/index.js
+++ src/components/views/index.js
@@ -1,5 +1,6 @@
 import Card from "@components/views/Card";
 import Page from "@components/views/Page";
 import Label from "@components/views/Label";
+import CenterModal from "@components/views/CenterModal";
 
-export { Card, Page, Label };
+export { Card, Page, Label, CenterModal };

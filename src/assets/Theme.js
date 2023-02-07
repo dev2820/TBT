74c24c07e7eacd5c41fdbac0c3640a766c72ad6a
dev2820 <second28200@gmail.com>
2023-02-07 11:28:36 +0900

    style/ Theme 스타일 생성

---
+++ src/assets/Theme.js
@@ -0,0 +1,10 @@
+import THEME from "@constants/THEME";
+import { StyleSheet } from "react-native";
+
+const theme = StyleSheet.create({
+  [THEME.READY]: {
+    backgroundColor: "#FCA14D",
+  },
+});
+
+export default theme;

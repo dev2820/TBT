aeeb44f6f58d56b2ecd7ec8f0097f5ed0a258993
dev2820 <second28200@gmail.com>
2023-02-07 20:17:29 +0900

    refactor/ 사용되지 않는 파라미터, 주석 제거

--- src/components/views/Card.js
+++ src/components/views/Card.js
@@ -1,21 +1,16 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
-const Card = ({ children, theme, style }) => {
+const Card = ({ children, style }) => {
   return <View style={[style, styles.container]}>{children}</View>;
 };
 
 const styles = StyleSheet.create({
   container: {
-    elevation: 6,
+    elevation: 5,
     borderRadius: 16,
     padding: 20,
   },
 });
 
 export default Card;
-
-/**
- * Button은 android와 ios가 다르게 보이기 때문에 TouchableOpacity를
- * 사용해 같은 UI로 보이게 만든다.
- */

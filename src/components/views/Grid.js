da4f2d3831359d0d5ee4d3e046699ab644662e1c
dev2820 <second28200@gmail.com>
2023-02-08 11:08:28 +0900

    refactor/Grid 컴포넌트 생성

---
+++ src/components/views/Grid.js
@@ -0,0 +1,25 @@
+import { View, FlatList } from "react-native";
+
+const Grid = ({ numColumns, renderItem, data }) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        style={styles.grid}
+        renderItem={renderItem}
+        numColumns={numColumns}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    width: "100%",
+  },
+  grid: {
+    padding: 10,
+  },
+});
+export default Grid;

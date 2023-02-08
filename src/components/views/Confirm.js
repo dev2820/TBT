09a76ae71c19c29bc1cc76377a45da127f9d2606
dev2820 <second28200@gmail.com>
2023-02-08 14:29:08 +0900

    style/Confirm 버튼 위치 수정, 크기 수정

--- src/components/views/Confirm.js
+++ src/components/views/Confirm.js
@@ -1,15 +1,39 @@
-import { View, Pressable, Text } from "react-native";
+import THEME from "@constants/THEME";
+import globalStyle from "@assets/globalStyle";
+import { View, Pressable, Text, StyleSheet } from "react-native";
 
 const Confirm = ({ children, onConfirm }) => {
   const CONFIRM = "확인";
   return (
-    <View>
-      {children}
-      <Pressable onPress={onConfirm}>
-        <Text>{CONFIRM}</Text>
+    <View style={styles.container}>
+      <View style={styles.content}>{children}</View>
+      <Pressable style={styles.confirm} onPress={onConfirm}>
+        <Text style={[globalStyle.TITLE_LARGE, styles.confirmText]}>
+          {CONFIRM}
+        </Text>
       </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    padding: 16,
+    flexGrow: 1,
+  },
+  confirm: {
+    height: 72,
+    alignItems: "center",
+    justifyContent: "center",
+    borderTopWidth: 1,
+    borderTopColor: THEME.PLACEHOLDER,
+  },
+  confirmText: {
+    color: THEME.PRIMARY,
+  },
+});
 
 export default Confirm;

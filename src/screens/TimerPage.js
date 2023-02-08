6cc3d47c640eaf3b5748a22d04b681c1184d9fec
dev2820 <second28200@gmail.com>
2023-02-08 22:52:30 +0900

    refactor/ 사용되지 않는 import 제거

--- src/screens/TimerPage.js
+++ src/screens/TimerPage.js
@@ -3,14 +3,9 @@ import { Text, View, StyleSheet, Button } from "react-native";
 import { Page } from "@components/views";
 import { observer } from "mobx-react-lite";
 import { observe } from "mobx";
-import { useSetupStore } from "@store/setupStore";
 import { useTimerStore } from "@store/timerStore";
 import { usePhaseStore } from "@store/phaseStore";
-import PHASE from "@constants/PHASE";
-import REPS from "@constants/REPS";
-import THEME from "@constants/THEME";
 
-const setup = useSetupStore();
 const phase = usePhaseStore();
 const timer = useTimerStore();
 

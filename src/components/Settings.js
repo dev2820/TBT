5548d11e5fc104ffad41fcf5f4a4032e357d0271
dev2820 <second28200@gmail.com>
2023-02-08 15:23:17 +0900

    refactor/Settings에서 Key에 따라 모달을 띄우도록 수정

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -3,20 +3,25 @@ import SettingCard from "@components/SettingCard";
 import { Grid } from "@components/views";
 import { useModalStore } from "@store/modalStore";
 import { observer } from "mobx-react-lite";
+import PHASE from "@constants/PHASE";
+import REPS from "@constants/REPS";
 
 const modalStore = useModalStore();
 
 const Settings = ({ settings }) => {
+  const showModal = (key) => {
+    if (key === PHASE.READY.NAME) {
+      modalStore.showReadySetupModal();
+      return;
+    }
+  };
   return (
     <View style={styles.container}>
       <Grid
         numColumns={2}
         data={settings}
         renderItem={({ item }) => (
-          <SettingCard
-            setting={item}
-            onPress={() => modalStore.showTimeModal()}
-          />
+          <SettingCard setting={item} onPress={() => showModal(item.key)} />
         )}
       ></Grid>
     </View>

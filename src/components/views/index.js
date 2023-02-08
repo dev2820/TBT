4ee56a77481d8dd8b16d3aeacb17af2cf3af3330
dev2820 <second28200@gmail.com>
2023-02-08 12:53:06 +0900

    feature/ flatButton export 추가

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -3,5 +3,6 @@ import Page from "@components/views/Page";
 import Label from "@components/views/Label";
 import CenterModal from "@components/views/CenterModal";
 import Grid from "@components/views/Grid";
+import FlatButton from "@components/views/FlatButton";
 
-export { Card, Page, Label, CenterModal, Grid };
+export { Card, Page, Label, CenterModal, Grid, FlatButton };

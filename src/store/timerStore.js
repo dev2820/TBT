42d79a394088fc2794548693f8d170cfc7965eab
dev2820 <second28200@gmail.com>
2023-02-08 20:54:17 +0900

    feature/타이머를 위한 timerStore 생성

---
+++ src/store/timerStore.js
@@ -0,0 +1,45 @@
+import { observable } from "mobx";
+
+const STATE = {
+  RUN: "run",
+  FINISH: "finish",
+};
+
+const timerStore = observable({
+  time: 0,
+  timer: null,
+  state: STATE.STOP,
+  setTime(time) {
+    this.time = time;
+  },
+  setTimer(newTimer) {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+    this.timer = newTimer;
+  },
+  setState(state) {
+    this.state = state;
+  },
+  run() {
+    this.setState(STATE.RUN);
+    this.setTimer(
+      setInterval(() => {
+        this.setTime(this.time - 1);
+        if (this.time === 0) {
+          this.setTimer(null);
+          this.setState(STATE.FINISH);
+        }
+      }, 1000)
+    );
+  },
+  clear() {
+    this.setTimer(null);
+  },
+});
+
+const useTimerStore = () => {
+  return timerStore;
+};
+
+export { useTimerStore };

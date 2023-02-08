5eeb8408b8abf6a53e52f74fb227acfcc7f0cc74
dev2820 <second28200@gmail.com>
2023-02-08 13:34:00 +0900

    refactor/ 사용되지 않는 FlatButton 제거

--- src/components/views/FlatButton.js
+++
@@ -1,7 +0,0 @@
-import { Pressable } from "react-native";
-
-const FlatButton = ({ onPress, children }) => {
-  return <Pressable onPress={onPress}>{children}</Pressable>;
-};
-
-export default FlatButton;

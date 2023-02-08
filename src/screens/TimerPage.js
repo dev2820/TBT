04ab60e9a403cfed979ddff7b27f2a2685db50f2
dev2820 <second28200@gmail.com>
2023-02-08 22:50:39 +0900

    feature/TimerPage가 mount되며 시작하고 phase가 끝나면 종료하도록 수정

--- src/screens/TimerPage.js
+++ src/screens/TimerPage.js
@@ -2,33 +2,39 @@ import React, { useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { Page } from "@components/views";
 import { observer } from "mobx-react-lite";
+import { observe } from "mobx";
 import { useSetupStore } from "@store/setupStore";
 import { useTimerStore } from "@store/timerStore";
+import { usePhaseStore } from "@store/phaseStore";
 import PHASE from "@constants/PHASE";
+import REPS from "@constants/REPS";
+import THEME from "@constants/THEME";
 
 const setup = useSetupStore();
+const phase = usePhaseStore();
 const timer = useTimerStore();
 
 const TimerPage = ({ navigation }) => {
-  useEffect(() => {
-    timer.setTime(setup[PHASE.READY.NAME].value);
-    timer.run();
-  }, []);
-
   const goBack = () => {
     timer.clear();
-    navigation.goBack();
+    navigation.pop();
   };
-
-  if (timer.time === 0) {
-    goBack();
-  }
+  useEffect(() => {
+    phase.run();
+    const diposer = observe(phase, "isFinished", ({ newValue }) => {
+      if (newValue === true) {
+        diposer();
+        goBack();
+      }
+    });
+  }, []);
 
   return (
-    <Page style={{ backgroundColor: "red" }}>
+    <Page style={{ backgroundColor: phase.currentPhase?.theme }}>
       <View style={styles.title}>
         <Button title="Home" onPress={goBack}></Button>
       </View>
+      <Text>{phase.currentPhase?.name}</Text>
       <Text>
         {timer.time} {timer.state}
       </Text>

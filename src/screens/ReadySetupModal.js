1dff06c20ec9c06f531ada5766ce6f56f7206662
dev2820 <second28200@gmail.com>
2023-02-08 14:59:30 +0900

    refactor/ TextInput이 숫자를 받을 수 있도록 세팅

--- src/screens/ReadySetupModal.js
+++ src/screens/ReadySetupModal.js
@@ -5,12 +5,15 @@ import { observer } from "mobx-react-lite";
 import { useModalStore } from "@store/modalStore";
 import { useSetupStore } from "@store/setupStore";
 import globalStyle from "@assets/globalStyle";
+import PHASE from "@constants/PHASE";
 
 const modal = useModalStore();
 const setup = useSetupStore();
 
 const ReadySetupModal = () => {
-  const [currentNum, setCurrentNum] = useState(3);
+  const [currentNum, changeCurrentNum] = useState(
+    setup[PHASE.READY.NAME].value
+  );
   const confirm = () => {
     setup.changeReadyTime(currentNum);
     modal.hideTimeModal();
@@ -23,8 +26,11 @@ const ReadySetupModal = () => {
       <Confirm onConfirm={confirm}>
         <Text style={[globalStyle.HEADING_LARGE, styles.title]}>준비 시간</Text>
         <TextInput
-          type="numberic"
-          onChangeText={(text) => setCurrentNum(parseInt(text), 10)}
+          style={{ height: 60 }}
+          onChangeText={changeCurrentNum}
+          value={currentNum}
+          defaultValue={currentNum.toString()}
+          keyboardType="numeric"
         ></TextInput>
       </Confirm>
     </CenterModal>

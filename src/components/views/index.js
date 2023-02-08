044b2a7c0d213b74d9b896a84810ca3e7bd64199
dev2820 <second28200@gmail.com>
2023-02-08 11:17:46 +0900

    refactor/ Grid 컴포넌트 export  추가

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -2,5 +2,6 @@ import Card from "@components/views/Card";
 import Page from "@components/views/Page";
 import Label from "@components/views/Label";
 import CenterModal from "@components/views/CenterModal";
+import Grid from "@components/views/Grid";
 
-export { Card, Page, Label, CenterModal };
+export { Card, Page, Label, CenterModal, Grid };

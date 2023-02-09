e9a16a1d2eb06b43661abc60594d6f41cecde09b
dev2820 <second28200@gmail.com>
2023-02-09 15:06:21 +0900

    refactor/BreakSetupModal을 TimePicker로 대체

--- src/screens/BreakSetupModal.js
+++ src/screens/BreakSetupModal.js
@@ -1,46 +1,31 @@
-import React, { useState } from "react";
-import { Text, StyleSheet, TextInput } from "react-native";
-import { CenterModal, Confirm } from "@components/views";
+import React from "react";
+import TimePickerModal from "@components/TimePickerModal";
 import { observer } from "mobx-react-lite";
 import { useModalStore } from "@store/modalStore";
 import { useSetupStore } from "@store/setupStore";
-import globalStyle from "@assets/globalStyle";
 import PHASE from "@constants/PHASE";
 
 const modal = useModalStore();
 const setup = useSetupStore();
 
 const BreakSetupModal = () => {
-  const [currentNum, changeCurrentNum] = useState(
-    setup[PHASE.BREAK.NAME].value
-  );
-  const confirm = () => {
-    setup.changeBreakTime(parseInt(currentNum || 0, 10));
+  const confirm = (time) => {
+    setup.changeBreakTime(time);
+    modal.hideBreakSetupModal();
+  };
+  const cancel = () => {
     modal.hideBreakSetupModal();
   };
   return (
-    <CenterModal
+    <TimePickerModal
+      title={"휴식 시간"}
       isVisible={modal.isBreakSetupModalVisible}
-      onBackdropPress={() => modal.hideBreakSetupModal()}
-    >
-      <Confirm onConfirm={confirm}>
-        <Text style={[globalStyle.HEADING_LARGE, styles.title]}>휴식 시간</Text>
-        <TextInput
-          style={{ height: 60 }}
-          onChangeText={changeCurrentNum}
-          value={currentNum}
-          defaultValue={currentNum.toString()}
-          keyboardType="numeric"
-        ></TextInput>
-      </Confirm>
-    </CenterModal>
+      onBackdropPress={cancel}
+      confirm={confirm}
+      cancel={cancel}
+      value={setup[PHASE.BREAK.NAME].value}
+    ></TimePickerModal>
   );
 };
-
-const styles = StyleSheet.create({
-  title: {
-    textAlign: "center",
-  },
-});
 
 export default observer(BreakSetupModal);

c4061d4072a66e09a3c61b1a08a8be28b0625425
dev2820 <second28200@gmail.com>
2023-02-09 12:55:45 +0900

    feature/ time picker 생성

--- src/screens/ReadySetupModal.js
+++ src/screens/ReadySetupModal.js
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
-import { Text, StyleSheet, TextInput } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 import { CenterModal, Confirm } from "@components/views";
 import { observer } from "mobx-react-lite";
 import { useModalStore } from "@store/modalStore";
 import { useSetupStore } from "@store/setupStore";
 import globalStyle from "@assets/globalStyle";
 import PHASE from "@constants/PHASE";
+import WheelPicker from "react-native-wheely";
 
 const modal = useModalStore();
 const setup = useSetupStore();
 
 const ReadySetupModal = () => {
-  const [currentNum, changeCurrentNum] = useState(
-    setup[PHASE.READY.NAME].value
+  const [currentMin, changeCurrentMin] = useState(
+    Math.floor(setup[PHASE.READY.NAME].value / 60)
+  );
+  const [currentSec, changeCurrentSec] = useState(
+    setup[PHASE.READY.NAME].value % 60
   );
   const confirm = () => {
-    setup.changeReadyTime(parseInt(currentNum || 0, 10));
+    setup.changeReadyTime(currentMin * 60 + currentSec);
     modal.hideReadySetupModal();
   };
   return (
@@ -25,13 +29,23 @@ const ReadySetupModal = () => {
     >
       <Confirm onConfirm={confirm}>
         <Text style={[globalStyle.HEADING_LARGE, styles.title]}>준비 시간</Text>
-        <TextInput
-          style={{ height: 60 }}
-          onChangeText={changeCurrentNum}
-          value={currentNum}
-          defaultValue={currentNum.toString()}
-          keyboardType="numeric"
-        ></TextInput>
+        <View style={styles.pickers}>
+          <View style={styles.picker}>
+            <WheelPicker
+              selectedIndex={currentMin}
+              options={[0, 1, 2, 3, 4, 5]}
+              onChange={(min) => changeCurrentMin(min)}
+            ></WheelPicker>
+          </View>
+          <Text style={{ textAlignVertical: "center" }}>:</Text>
+          <View style={styles.picker}>
+            <WheelPicker
+              selectedIndex={currentSec}
+              options={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+              onChange={(sec) => changeCurrentSec(sec)}
+            ></WheelPicker>
+          </View>
+        </View>
       </Confirm>
     </CenterModal>
   );
@@ -40,6 +54,14 @@ const ReadySetupModal = () => {
 const styles = StyleSheet.create({
   title: {
     textAlign: "center",
+  },
+  pickers: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  picker: {
+    width: 60,
+    marginHorizontal: 10,
   },
 });
 

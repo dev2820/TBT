cc6e55983b5fa572afae32d8c6f8a6e10b3c8c84
dev2820 <second28200@gmail.com>
2023-02-09 15:12:14 +0900

    feature/ReadySetupModal이 TimePickerModal을 사용하도록 수정

--- src/screens/ReadySetupModal.js
+++ src/screens/ReadySetupModal.js
@@ -1,52 +1,30 @@
-import React, { useState } from "react";
-import { Text, StyleSheet, View } from "react-native";
-import { CenterModal, Confirm } from "@components/views";
-import TimePicker from "@components/TimePicker";
+import React from "react";
+import TimePickerModal from "@components/TimePickerModal";
 import { observer } from "mobx-react-lite";
 import { useModalStore } from "@store/modalStore";
 import { useSetupStore } from "@store/setupStore";
-import globalStyle from "@assets/globalStyle";
-import createDebounce from "@utils/createDebounce";
 import PHASE from "@constants/PHASE";
 
 const modal = useModalStore();
 const setup = useSetupStore();
 
 const ReadySetupModal = () => {
-  const [currentTime, setCurrentTime] = useState(setup[PHASE.READY.NAME].value);
-  const debouncedTimerSetter = createDebounce(setCurrentTime);
-  const confirm = () => {
-    setup.changeReadyTime(currentTime);
+  const confirm = (time) => {
+    setup.changeReadyTime(time);
     modal.hideReadySetupModal();
   };
   const cancel = () => {
     modal.hideReadySetupModal();
   };
   return (
-    <CenterModal
+    <TimePickerModal
+      title={"준비 시간"}
       isVisible={modal.isReadySetupModalVisible}
-      onBackdropPress={cancel}
-    >
-      <Confirm onConfirm={confirm} onCancel={cancel}>
-        <Text style={[globalStyle.HEADING_SMALL, styles.title]}>준비 시간</Text>
-        <TimePicker
-          style={styles.picker}
-          timeSelected={currentTime}
-          onChange={debouncedTimerSetter}
-        ></TimePicker>
-      </Confirm>
-    </CenterModal>
+      confirm={confirm}
+      cancel={cancel}
+      value={setup[PHASE.BREAK.NAME].value}
+    ></TimePickerModal>
   );
 };
-
-const styles = StyleSheet.create({
-  title: {
-    textAlign: "center",
-    fontWeight: "700",
-  },
-  picker: {
-    justifyContent: "center",
-  },
-});
 
 export default observer(ReadySetupModal);

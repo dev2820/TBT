3fb6bd85c6b24753c5c016e2dfdddb20273dc00c
dev2820 <second28200@gmail.com>
2023-02-09 14:11:32 +0900

    style/ TimePicker 폰트 사이즈 조절

--- src/components/TimePicker.js
+++ src/components/TimePicker.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import WheelPicker from "react-native-wheely";
 import TIME from "@constants/TIME";
+import globalStyle from "@assets/globalStyle";
 
 const TimePicker = ({ style, timeSelected, onChange }) => {
   const [currentMin, changeCurrentMin] = useState(
@@ -16,6 +17,9 @@ const TimePicker = ({ style, timeSelected, onChange }) => {
   return (
     <View style={[styles.pickers, style]}>
       <WheelPicker
+        scaleFunction={(x) => Math.pow(3 / 4, x)}
+        itemTextStyle={globalStyle.HEADING_LARGE}
+        itemHeight={50}
         selectedIndex={currentMin}
         containerStyle={styles.picker}
         options={TIME.MIN}
@@ -23,6 +27,9 @@ const TimePicker = ({ style, timeSelected, onChange }) => {
       ></WheelPicker>
       <Text style={styles.colon}>:</Text>
       <WheelPicker
+        scaleFunction={(x) => Math.pow(3 / 4, x)}
+        itemTextStyle={globalStyle.HEADING_LARGE}
+        itemHeight={50}
         selectedIndex={currentSec}
         containerStyle={styles.picker}
         options={TIME.SEC}
@@ -37,7 +44,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   picker: {
-    width: 60,
+    width: 80,
     marginHorizontal: 10,
   },
   colon: {

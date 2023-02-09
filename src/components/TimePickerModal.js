e47ebe97ec7b7a35c1161d53447655996cc52ec6
dev2820 <second28200@gmail.com>
2023-02-09 15:06:03 +0900

    feature/TimePicker 생성

---
+++ src/components/TimePickerModal.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Text, StyleSheet } from "react-native";
+import { CenterModal, Confirm } from "@components/views";
+import TimePicker from "@components/TimePicker";
+import globalStyle from "@assets/globalStyle";
+import createDebounce from "@utils/createDebounce";
+
+const TimePickerModal = ({
+  title,
+  isVisible,
+  onBackdropPress,
+  confirm,
+  cancel,
+  value,
+}) => {
+  const [currentTime, changeCurrentTime] = useState(value);
+  const debouncedTimerSetter = createDebounce(changeCurrentTime);
+
+  return (
+    <CenterModal isVisible={isVisible} onBackdropPress={onBackdropPress}>
+      <Confirm
+        onConfirm={() => {
+          confirm(currentTime);
+        }}
+        onCancel={() => {
+          changeCurrentTime(value);
+          cancel();
+        }}
+      >
+        <Text style={[globalStyle.HEADING_SMALL, styles.title]}>{title}</Text>
+        <TimePicker
+          style={styles.picker}
+          timeSelected={currentTime}
+          onChange={debouncedTimerSetter}
+        ></TimePicker>
+      </Confirm>
+    </CenterModal>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontWeight: "700",
+  },
+  picker: {
+    justifyContent: "center",
+  },
+});
+
+export default TimePickerModal;

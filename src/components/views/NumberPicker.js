3454d877379fcf3c0aae81beef91466ff94bec9c
dev2820 <second28200@gmail.com>
2023-02-09 19:33:37 +0900

    style&feature/ max보다 커지거나 min보다 작아지지 않게 설정함

--- src/components/views/NumberPicker.js
+++ src/components/views/NumberPicker.js
@@ -1,5 +1,7 @@
-import { useState } from "react";
-import { TouchableHighlight, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { TouchableHighlight, View, Text, StyleSheet } from "react-native";
+import THEME from "@constants/THEME";
+import globalStyle from "@assets/globalStyle";
 
 const NumberPicker = ({ initNum, max = 0, min = 0, step, onChange }) => {
   const [num, setNum] = useState(initNum);
@@ -7,26 +9,61 @@ const NumberPicker = ({ initNum, max = 0, min = 0, step, onChange }) => {
   const [isDecreaseDisable, setDecreaseDisable] = useState(num + step < min);
   const increase = () => {
     setNum(num + step);
-    setIncreaseDisable(num + step > max);
     onChange(num);
   };
   const decrease = () => {
     setNum(num - step);
-    setDecreaseDisable(num - step > min);
     onChange(num);
   };
+  useEffect(() => {
+    setIncreaseDisable(num + step > max);
+    setDecreaseDisable(num - step < min);
+  }, [num]);
 
   return (
     <View>
-      <TouchableHighlight onPress={increase} disabled={isIncreaseDisable}>
-        <Text>+</Text>
+      <TouchableHighlight
+        style={styles.button}
+        onPress={increase}
+        disabled={isIncreaseDisable}
+      >
+        <Text
+          style={[globalStyle.LABEL_LARGE, isIncreaseDisable && styles.disable]}
+        >
+          +
+        </Text>
       </TouchableHighlight>
-      <Text>{num}</Text>
-      <TouchableHighlight onPress={decrease} disabled={isDecreaseDisable}>
-        <Text>-</Text>
+      <Text style={[styles.number, globalStyle.HEADING_LARGE]}>{num}</Text>
+      <TouchableHighlight
+        style={styles.button}
+        onPress={decrease}
+        disabled={isDecreaseDisable}
+      >
+        <Text
+          style={[globalStyle.LABEL_LARGE, isDecreaseDisable && styles.disable]}
+        >
+          -
+        </Text>
       </TouchableHighlight>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  button: {
+    width: 80,
+    height: 40,
+    backgroundColor: THEME.PLACEHOLDER,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  number: {
+    textAlign: "center",
+    marginVertical: 24,
+  },
+  disable: {
+    opacity: 0.5,
+  },
+});
 export default NumberPicker;

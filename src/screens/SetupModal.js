ac54d6baf55680ea4dca893012edbdfd8c6a8356
dev2820 <second28200@gmail.com>
2023-02-17 11:24:22 +0900

    refactor/ SetupModal에서 사용하는 TimePickerModal과 NumberPickerModal을 SetupModal에 합침

--- src/screens/SetupModal.js
+++ src/screens/SetupModal.js
@@ -1,53 +1,84 @@
-import React from "react";
-import TimePickerModal from "@components/TimePickerModal";
-import NumberPickerModal from "@components/NumberPickerModal";
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet } from "react-native";
+import TimePicker from "@components/TimePicker";
+import { CenterModal, Confirm, NumberPicker } from "@components/views";
 import { observer } from "mobx-react-lite";
 import { useModalStore } from "@store/modalStore";
 import { useSetupStore } from "@store/setupStore";
 import SETTING from "@constants/SETTING";
+import globalStyle from "@assets/globalStyle";
 
 const modal = useModalStore();
 const setup = useSetupStore();
 
-const confirm = (time) => {
-  setup.changeCurrentSetupValue(time);
-  modal.hideModal();
-};
-const cancel = () => {
-  modal.hideModal();
-};
-
 const SetupModal = () => {
-  if (setup.currentSetup.type === SETTING.TYPE.TIME) {
-    return TimePicker();
-  }
+  const originalValue = setup.currentSetup.value;
+  const title = setup.currentSetup.name;
+  const pickerType = setup.currentSetup.type;
+  const [setupValue, changeSetupValue] = useState(originalValue);
+  useEffect(() => {
+    if (!modal.isModalVisible) return;
+    changeSetupValue(setup.currentSetup.value);
+  }, [setup.currentSetup.value, modal.isModalVisible]);
 
-  return NumberPicker();
-};
+  const confirm = (time) => {
+    setup.changeCurrentSetupValue(time);
+    modal.hideModal();
+  };
+  const cancel = () => {
+    modal.hideModal();
+  };
 
-const TimePicker = () => {
   return (
-    <TimePickerModal
-      title={setup.currentSetup.name}
-      isVisible={modal.isModalVisible}
-      confirm={confirm}
-      cancel={cancel}
-      value={setup.currentSetup.value}
-    ></TimePickerModal>
+    <CenterModal isVisible={modal.isModalVisible} onBackdropPress={cancel}>
+      <Confirm
+        onConfirm={() => {
+          confirm(setupValue);
+        }}
+        onCancel={cancel}
+      >
+        <Text style={[globalStyle.HEADING_SMALL, styles.title]}>{title}</Text>
+        {pickerType === SETTING.TYPE.TIME
+          ? TimePickerView(setupValue, changeSetupValue)
+          : NumberPickerView(setupValue, changeSetupValue)}
+      </Confirm>
+    </CenterModal>
   );
 };
 
-const NumberPicker = () => {
+const TimePickerView = (value, onChange) => {
   return (
-    <NumberPickerModal
-      title={setup.currentSetup.name}
-      isVisible={modal.isModalVisible}
-      confirm={confirm}
-      cancel={cancel}
+    <TimePicker
+      style={styles.picker}
+      timeSelected={value}
+      onChange={(time) => onChange(time)}
+    ></TimePicker>
+  );
+};
+const NumberPickerView = (value, onChange) => {
+  return (
+    <NumberPicker
+      style={styles.picker}
+      initNum={value}
+      max={99}
+      min={0}
+      step={1}
       isRotatable={true}
-      value={setup.currentSetup.value}
-    ></NumberPickerModal>
+      onChange={(num) => onChange(num)}
+    ></NumberPicker>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontWeight: "700",
+  },
+  picker: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+});
 
 export default observer(SetupModal);
